bda69e530a1c45a3ae46c10b93b8a35b32bf9a7a
Drumbler <andy132012@gmail.com>
2023-10-02 14:35:00 +0500

    Initial commit

---
+++ src/main/java/com/example/lab_7/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.lab_7.repository;
+
+import com.example.lab_7.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+}
